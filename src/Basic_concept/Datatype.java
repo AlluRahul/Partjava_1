9ad8448af3708d5d439b9e9fba2b70070216266c
AlluRahul <allurahul57@gmail.com>
2024-03-20 11:47:04 +0530

    final

---
+++ src/Basic_concept/Datatype.java
@@ -0,0 +1,19 @@
+package Basic_concept;
+
+class Datatype
+{
+	public static void main(String[] args) 
+	{
+		int number =20;
+		double decimal =10.5;
+		char character = 'c';
+		String message = "hi";
+		boolean status = false;
+		System.out.println(number); 
+		System.out.println(decimal);
+		System.out.println(character);
+		System.out.println(message);
+		System.out.println(status);
+	}
+}
+
